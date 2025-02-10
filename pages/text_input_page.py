9e044fab2453fe5e6b0fc73944f4364501947b58
Maxim Danilov <abl1v1on@yandex.ru>
2025-02-10 21:15:31 +0500

    add func for fill and send form

--- pages/text_input_page.py
+++ pages/text_input_page.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from selenium.webdriver.ie.webdriver import WebDriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webelement import WebElement
 
 from . import locator
@@ -16,7 +17,10 @@ class TextInputPage(BaseInputPage):
     def __init__(self, browser: WebDriver, timeout: int = 5) -> None:
         super().__init__(browser, timeout)
         self.url = self.url + '/simple'
-    
+
     @property
     def text_field(self) -> WebElement:
         return self.find(*TextInputPageLocator.text_field)
+
+    def fill_and_press_enter(self, text: str) -> None:
+        self.text_field.send_keys(text + Keys.ENTER)

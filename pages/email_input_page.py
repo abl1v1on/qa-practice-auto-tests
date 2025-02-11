4cab0332a63ba9e02f18faf813a68462c2e16bd9
Maxim Danilov <abl1v1on@yandex.ru>
2025-02-11 11:27:02 +0500

    add fill and press enter method

--- pages/email_input_page.py
+++ pages/email_input_page.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from selenium.webdriver.ie.webdriver import WebDriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webelement import WebElement
 
 from . import locator
@@ -20,3 +21,7 @@ class EmailInputPage(BaseInputPage):
     @property
     def email_field(self) -> WebElement:
         return self.find(*EmailInputPageLocator.email_field)
+
+    # TODO: вынеси в базовый класс инпутов
+    def fill_and_press_enter(self, email: str) -> None:
+        self.email_field.send_keys(email + Keys.ENTER)

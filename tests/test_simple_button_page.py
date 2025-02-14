1c721ee48a772b19bfa25c71686f5317d3ebf203
Maxim Danilov <abl1v1on@yandex.ru>
2025-02-14 13:06:44 +0500

    add tests for simple button page

---
+++ tests/test_simple_button_page.py
@@ -0,0 +1,17 @@
+import pytest
+from typing import Generator
+from selenium.webdriver.ie.webdriver import WebDriver
+
+from pages.buttons.simple_button_page import SimpleButtonPage
+
+
+@pytest.fixture(scope='function')
+def page(browser: WebDriver) -> Generator[SimpleButtonPage, None, None]:
+    page = SimpleButtonPage(browser=browser)
+    page.open()
+    yield page
+
+
+def test_simple_button_result_block_is_displayed(page: SimpleButtonPage) -> None:
+    page.click_to_btn()
+    assert page.check_result_text('Submitted'), 'Неправильное отображение результата'

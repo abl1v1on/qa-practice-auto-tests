b7a2b2fde49bb93da6528fde61f4bc5c73fd2d25
Maxim Danilov <abl1v1on@yandex.ru>
2025-02-14 14:15:48 +0500

    add disabled button page

---
+++ pages/buttons/disabled_button_page.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from selenium.webdriver.ie.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.ui import Select
+
+from pages import locator
+from pages.buttons.base_button_page import BaseButtonPage
+
+
+@dataclass(frozen=True)
+class DisabledButtonPageLocator:
+    button_state_selector: locator = (By.ID, 'id_select_state')
+
+
+class DisabledButtonPage(BaseButtonPage):
+    def __init__(self, browser: WebDriver, timeout: int = 5) -> None:
+        super().__init__(browser, timeout)
+    
+    @property
+    def disabled_button(self) -> WebElement:
+        return super().button
+
+    @property
+    def button_state_selector(self) -> WebElement:
+        return self.find(*DisabledButtonPageLocator.button_state_selector)
+
+    def select_state(self, value: str) -> None:
+        select = Select(self.button_state_selector)
+        select.select_by_value(value)
